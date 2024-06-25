108c5494e3b04f4fc4275a9254e1375546a07e94
dax-reo-takahara <reo.takahara@data-x.com>
2024-06-25 21:03:53 +0900

    色々変更中

---
+++ src/components/lv2/List.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+.front-list
+
+</template>
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  name: "List",
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
